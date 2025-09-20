75bc926b3eb692b7cc84413eb20361ee2312e260
thoxbox <sojobees@gmail.com>
2025-09-20 13:15:08 -0600

    Made lascra's error/success messages visible to the console. Also formatted the document with Prettier.

--- src/index.js
+++ src/index.js
@@ -4,8 +4,12 @@ const child_process = require("child_process");
 function outputCode(code) {
     fs.writeFileSync(`build\\output.scra`, code, "utf-8");
     child_process.exec(
-        `lascra Sprite1.sprite3 output.scra`
-        , { cwd: "build" });
+        `lascra Sprite1.sprite3 output.scra`,
+        { cwd: "build" },
+        (_, stdout) => {
+            console.log(stdout);
+        }
+    );
 }
 
 outputCode(`(when flag
