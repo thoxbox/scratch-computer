49e752de904fffda4c880637fa2035147a70cca2
thoxbox <sojobees@gmail.com>
2025-09-28 16:10:19 -0600

    Made the switch functions take in entries instead of taking an object. Made the code more readable.

--- src/index.js
+++ src/index.js
@@ -2,19 +2,19 @@ import outputCode from "./outputCode.js";
 import Top from "./Top.js";
 
 function lc_switch(value, against) {
-    return Object.entries(against)
-        .map((x, i, arr) => {
-            const doBreak = !(i + 1 === arr.length || x[1].includes("(stop "));
-            return `(if (= ${value} ${x[0]})
-                ${x[1]}
+    return against
+        .map(([key, value], i, arr) => {
+            const doBreak = !(i + 1 === arr.length || value.includes("(stop "));
+            return `(if (= ${value} ${key})
+                ${value}
                 ${doBreak ? '(stop "this script")' : ""}
             )`;
         })
         .join("\n");
 }
 function lc_broadcast_switch(value, against, wait = false) {
-    Object.entries(against).forEach((x) => {
-        Top.broadcast(x[0], x[1]);
+    against.forEach(([key, value]) => {
+        Top.broadcast(key, value);
     });
     return `(${wait ? "broadcast_wait" : "broadcast"} ${value})`;
 }
@@ -33,9 +33,9 @@ Top.procedure("run_program", "", `
     )
     (set pc 1)
     (forever
-        ${lc_broadcast_switch("(item (variable pc) memory)", {
-            halt: "(stop all)",
-        })}
+        ${lc_broadcast_switch("(item (variable pc) memory)", [
+            ["halt", "(stop all)"],
+        ])}
     )
 `);
 outputCode(Top.output());
